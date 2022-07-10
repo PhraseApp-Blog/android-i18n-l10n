031ec1cfa1c3a0fd3e606c94c447af74fe759e51
Shivam Dhuria <shivam.dhuria07@gmail.com>
2022-07-10 19:58:16 +0530

    added psudi locales

--- app/src/main/java/com/elixer/phrase/MainActivity.kt
+++ app/src/main/java/com/elixer/phrase/MainActivity.kt
@@ -6,25 +6,12 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.elixer.phrase.ui.theme.PhraseTheme
-import java.time.Instant
-import java.time.ZoneId
-import java.time.ZonedDateTime
-import java.time.format.DateTimeFormatter
-import java.time.format.FormatStyle
 
 @OptIn(ExperimentalMaterialApi::class)
 class MainActivity : ComponentActivity() {
@@ -38,20 +25,18 @@ class MainActivity : ComponentActivity() {
           modifier = Modifier.fillMaxSize(),
           color = MaterialTheme.colors.background
         ) {
-
-          Row(
-            verticalAlignment = Alignment.CenterVertically, modifier = Modifier.padding(
-              top =
-              20.dp
-            )
-          ) {
-            Spacer(modifier = Modifier.weight(1f))
+          Column(Modifier.padding(top = 200.dp), horizontalAlignment = Alignment.CenterHorizontally) {
             Text(
-              text =  getString(R.string.landing_url),
+              text = getString(R.string.landing_url),
               fontSize = 20.sp,
               modifier = Modifier.padding(start = 10.dp)
             )
-            Spacer(modifier = Modifier.weight(1f))
+            Button(onClick = {}) {
+              Text(text = getString(R.string.next))
+            }
+            Button(onClick = {}) {
+              Text(text = "Back")
+            }
           }
         }
       }
